a40edfb18b81ccbac7fe2c106a8e98b1b85bb55f
RuslanWorkGit <lulka758@gmail.com>
2022-07-22 14:12:39 +0300

    Done task with Function

---
+++ SomeTask/Function/Functions03.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//Створіть функцію яка приймає параметри і вичисляє площу круга.
+
+func circleArea (radius: Double) {
+    print("Circle area is \(2 * radius * Double.pi)")
+}
+
+circleArea(radius: 5)
+circleArea(radius: 10)
+circleArea(radius: 29)

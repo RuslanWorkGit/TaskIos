eb29bc82c542f15e55778cbfa669eaadd8401065
RuslanWorkGit <lulka758@gmail.com>
2022-07-31 22:05:44 +0300

    HomeWork3 final

--- Block3/Homework3/Homework3.playground/Contents.swift
+++ Block3/Homework3/Homework3.playground/Contents.swift
@@ -4,7 +4,8 @@ import UIKit
 
 // Task 2 - https://github.com/RuslanWorkGit/TaskIos/blob/master/Block3/Protocols/MyOwnTask/MyTask.playground/Contents.swift
 
-// У проекті xCode - додав щоб при натисканні кнопки змінювавсуя background color і коли натискаєш на 1 кнопку то воно чомусь змінює колір на другій кнопці.  https://github.com/RuslanWorkGit/TaskIos/blob/0d3947c58dfea94058762f244b42c4da49a2e183/Block3/Protocols/xCodeProj/ProtocolOpt/ProtocolOpt/ViewController.swift
+// У проекті xCode - додав щоб при натисканні кнопки змінювавсуя background color і коли натискаєш на 1 кнопку то воно чомусь змінює колір на другій кнопці.
+// https://github.com/RuslanWorkGit/TaskIos/blob/0d3947c58dfea94058762f244b42c4da49a2e183/Block3/Protocols/xCodeProj/ProtocolOpt/ProtocolOpt/ViewController.swift
 
 // Task 3
 
@@ -50,8 +51,10 @@ text2.isPolindrome()
 let text3 = "арозаупаланалапуазора"
 text3.isPolindrome()
 
-// Task 4 - додав щоб при натисканні кнопки змінювавсуя background color і коли натискаєш на 1 кнопку то воно чомусь змінює колір на другій кнопці.  https://github.com/RuslanWorkGit/TaskIos/blob/master/Block3/Protocols/xCodeProj/ProtocolOpt/ProtocolOpt/ViewController.swift
+// Task 4 - додав щоб при натисканні кнопки змінювавсуя background color і коли натискаєш на 1 кнопку то воно чомусь змінює колір на другій кнопці.
+// https://github.com/RuslanWorkGit/TaskIos/blob/master/Block3/Protocols/xCodeProj/ProtocolOpt/ProtocolOpt/ViewController.swift
 
 // Task 5 - https://github.com/RuslanWorkGit/TaskIos/blob/0d3947c58dfea94058762f244b42c4da49a2e183/Block3/Generic/Generic.playground/Contents.swift
 
-//Task 6 - взяв з уроку SwiftBook, зрозумів як це працює можливо потім зроблю якось сам але то мабуть будуть вже типу костилі. І коли я вирішив створити посилання на минулий щоб зберігалося посилання і на анступний і на минулий то воно писало що буде цикл і помилка, не зовсім зрозумів як це виправити. https://github.com/RuslanWorkGit/TaskIos/blob/master/Block3/Homework3/LinkedList/Linked_list.playground/Contents.swift
+//Task 6 - взяв з уроку SwiftBook, зрозумів як це працює можливо потім зроблю якось сам але то мабуть будуть вже типу через костилі так сказати. І коли я вирішив створити посилання на минулий щоб зберігалося посилання і на анступний і на минулий то воно писало що буде цикл і помилка, не зовсім зрозумів як це виправити.
+// https://github.com/RuslanWorkGit/TaskIos/blob/master/Block3/Homework3/LinkedList/Linked_list.playground/Contents.swift

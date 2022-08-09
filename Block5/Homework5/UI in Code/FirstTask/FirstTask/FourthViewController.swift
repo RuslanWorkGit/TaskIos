eabdf79332a57257f852cbb1d6609a4df9dc5ead
RuslanWorkGit <lulka758@gmail.com>
2022-08-09 22:10:44 +0300

    Add done third storyboard and not full fourth

--- Block5/Homework5/UI in Code/FirstTask/FirstTask/FourthViewController.swift
+++ Block5/Homework5/UI in Code/FirstTask/FirstTask/FourthViewController.swift
@@ -8,22 +8,45 @@
 import UIKit
 
 class FourthViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addCentralCircle()
+        
+ 
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addCentralCircle(){
+        let circleMain = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.width)
+        let mainCircle = MyView(frame: circleMain)
+        mainCircle.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        mainCircle.backgroundColor = .gray
+        view.addSubview(mainCircle)
+        
+        func addRedCircle() {
+            let circle = CGRect(x: view.bounds.minX, y: view.bounds.midY - 220, width: view.bounds.width * 0.65, height: view.bounds.width * 0.65)
+            let redCircle = MyView(frame: circle)
+            redCircle.backgroundColor = .red
+            mainCircle.addSubview(redCircle)
+        }
+        addRedCircle()
+        
+        func addYellowCircle() {
+            
+        }
+        
+        func addGreenCircle() {
+            
+        }
+        
+        func addBlueCircle() {
+            
+        }
     }
-    */
+    
+   
+    
 
 }
--- Block5/Homework5/UI in Code/FirstTask/FirstTask/ThirdViewController.swift
+++ Block5/Homework5/UI in Code/FirstTask/FirstTask/ThirdViewController.swift
@@ -13,23 +13,32 @@ class ThirdViewController: UIViewController {
         super.viewDidLoad()
 
         addRedCircle()
-        addYellowCircle()
         addGreenCircle()
+        addYellowCircle()
+
     }
 
     func addRedCircle() {
-        let circle = CGRect(x: view.bounds.midX, y: view.bounds.midY * 0.5, width: view.bounds.width / 1.5, height: view.bounds.width / 1.5)
+        let circle = CGRect(x: view.bounds.midX - 50, y: view.bounds.midY * 0.4, width: view.bounds.width / 1.5, height: view.bounds.width / 1.5)
         let redCircle = MyView(frame: circle)
         redCircle.backgroundColor = .red
         view.addSubview(redCircle)
     }
     
     func addYellowCircle() {
-        
+        let circle = CGRect(x: view.bounds.midX, y: view.bounds.midY, width: view.bounds.width / 1.5, height: view.bounds.width / 1.5)
+        let yellowCircle = MyView(frame: circle)
+        yellowCircle.backgroundColor = .systemYellow
+        yellowCircle.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        yellowCircle.alpha = 0.7
+        view.addSubview(yellowCircle)
     }
     
     func addGreenCircle() {
-        
+        let circle = CGRect(x: view.bounds.minX, y: view.bounds.midY + 40, width: view.bounds.width / 1.5, height: view.bounds.width / 1.5)
+        let greenCircle = MyView(frame: circle)
+        greenCircle.backgroundColor = .systemGreen
+        view.addSubview(greenCircle)
     }
     
 
